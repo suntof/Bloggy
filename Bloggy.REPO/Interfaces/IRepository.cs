4a66e49822f0333e48f8caaf661c5a4c127e8d9d
suntof <mustafasand@hotmail.com>
2024-01-23 00:15:16 +0300

    dependency injection

--- Bloggy.REPO/Interfaces/IRepository.cs
+++ Bloggy.REPO/Interfaces/IRepository.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Bloggy.CORE.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace Bloggy.REPO.Interfaces
 {
-	public interface IRepository
+	public interface IRepository<T> where T : class, IBaseEntity, new()
 	{
-
+		Task AddAsync(T entity);
+		Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+		Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+		Task<T> GetByGuidAsync(Guid id);
+		Task<T> UpdateAsync(T entity);
+		Task DeleteAsync(T entity);
+		Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+		Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
 	}
 }
